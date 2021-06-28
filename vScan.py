ad68c6612351fb3761208fe18042a960196a6998
xTeKc <bcd3v@pm.me>
2021-06-27 23:08:15 -0400

    extract links func

--- vScan.py
+++ vScan.py
@@ -1,7 +1,10 @@
 import requests
+import re
 
 class vScan:
     def __init__(self, url):
         self.target_url = url
     
-    def extract_links_from(self, )
+    def extract_links_from(self, url):
+        response = requests.get(url)
+        return re.findAll('(?:href=")(.*?)"', response.content)
