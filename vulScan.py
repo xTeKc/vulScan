0c4029b486a21d86613504a1a55ea2c86da4dd2b
xTeKc <bcd3v@pm.me>
2021-06-27 23:30:12 -0400

    update filename

--- vulScan.py
+++ vulScan.py
@@ -1,8 +1,8 @@
-import vScan
+import vScanConf
 
 #input target webpage url
 target_url = 'webpage-url'
-vuln_scanner = vScan.ScanV(target_url)
+vuln_scanner = vScanConf.ScanV(target_url)
 vuln_scanner.crawl(target_url)
 
 
