ab83178d7be90cb28310e362d15155c33b3117a8
xTeKc <bcd3v@pm.me>
2021-06-27 20:35:22 -0400

    update content

--- vulScan.py
+++ vulScan.py
@@ -7,9 +7,13 @@ def request(url):
     except requests.exceptions.ConnectionError:
         pass
 
+#get html from webpage
 target_url = 'enter-webpage-url'
 response = request(target_url)
+#print(response.content)
 
+#parse diff elements of html on webpage
 parsed_html = BeautifulSoup(response.content)
 forms_list = parsed_html.findAll('form-on-page')
 print(forms_list)
+
