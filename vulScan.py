9de381f9b1c2861507750fd611407903e61b4a3d
xTeKc <bcd3v@pm.me>
2021-07-26 16:00:18 -0400

    refactor

--- vulScan.py
+++ vulScan.py
@@ -1,7 +1,7 @@
 import vScanConf
 
 #input target webpage url
-target_url = 'webpage-url'
+target_url = 'http://duckduckgo.com'
 vuln_scanner = vScanConf.ScanV(target_url)
 vuln_scanner.crawl(target_url)
 
