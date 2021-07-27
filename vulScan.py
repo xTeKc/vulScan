4e2f4dcb01694ffd4416f5203488cfcf3e428841
xTeKc <bcd3v@pm.me>
2021-07-26 20:49:08 -0400

    comment where to put new links to ignore arg

--- vulScan.py
+++ vulScan.py
@@ -7,7 +7,7 @@ target_url = input(f"Enter WebPage URL: ")
 
 data_dict = {"username": "admin", "password": "password", "Login": "submit"}  #login session
 
-vuln_scanner = vScanConf.ScanV(target_url)
+vuln_scanner = vScanConf.ScanV(target_url)  #, links_to_ignore  #add links_to_ignore as arg
 response = vuln_scanner.session.post(target_url, data = data_dict)  #response to session
 #vuln_scanner.session.post(input(f"Enter WebPage URL: "), data = data_dict)  #refactored version
 
