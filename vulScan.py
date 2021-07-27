5f61bc930550f454c04c727b3e2a832051b31a06
xTeKc <bcd3v@pm.me>
2021-07-26 20:34:10 -0400

    describe lines

--- vulScan.py
+++ vulScan.py
@@ -3,10 +3,10 @@ import requests
 
 #input target webpage url
 target_url = input(f"Enter WebPage URL: ")
-data_dict = {"username": "admin", "password": "password", "Login": "submit"}
+data_dict = {"username": "admin", "password": "password", "Login": "submit"}  #login session
 
 vuln_scanner = vScanConf.ScanV(target_url)
-response = vuln_scanner.session.post(target_url, data = data_dict)
+response = vuln_scanner.session.post(target_url, data = data_dict)  #response to session
 
 vuln_scanner.crawl()
 
