4788fadc83180222a34a9ecc8878591de2917efb
xTeKc <bcd3v@pm.me>
2021-07-26 20:27:09 -0400

    add login session

--- vulScan.py
+++ vulScan.py
@@ -1,7 +1,10 @@
 import vScanConf
 
 #input target webpage url
-target_url = input('Enter WebPage URL: ')
+target_url = input(f"Enter WebPage URL: ")
+data_dict = {"username": "admin", "password": "password", "Login": "submit"}
+response = requests.post(target_url, data = data_dict)
+
 vuln_scanner = vScanConf.ScanV(target_url)
 vuln_scanner.crawl()
 
