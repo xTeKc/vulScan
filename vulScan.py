6f4f326d461825c581427d6e4f698f36b74b4d37
xTeKc <bcd3v@pm.me>
2021-07-26 23:49:26 -0400

    add more comments for different config

--- vulScan.py
+++ vulScan.py
@@ -15,9 +15,13 @@ vuln_scanner.crawl()
 
 #forms = vuln_scanner.extract_form(input(f"Enter WebPage URL: "))  #extract_form from webpage
 #print(forms)  #print the forms from webpage
-#response = vuln_scanner.submit_form(forms[0], "test-test", input(f"Enter WebPage URL: "))
+#response = vuln_scanner.submit_form(forms[0], "test-test", input(f"Enter WebPage URL: "))  #enter url
 #print(response.content)  #print test-test into form on entered webpage
 
-#replace with currenct response if needed
-#response = vuln_scanner.test_xss_in_form(forms[0], input(f"Enter WebPage URL: "))
-#print(response)  #print response for xss vulnerability testing
+#replace with current response if needed
+#response = vuln_scanner.test_xss_in_form(forms[0], input(f"Enter WebPage URL: "))  #enter url
+#print(response)  #print response for xss form vulnerability testing
+
+#replace with current response if needed
+#response = vuln_scanner.test_xss_in_link(input(f"Enter WebPage URL: "))  #enter url
+#print(response)  #print response for xss link vulnerability testing
